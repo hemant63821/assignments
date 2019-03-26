4ee44c76667ac8016c927834890af1507885118e
hemant.gogoi <hemant.gogoi@credr.com>
2019-03-26 21:06:21 +0530

    Feat() : initial header and footer setup for the design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import './style.scss';
+import Header from './Components/Header/Header.js'
+import Footer from './Components/Footer/Footer.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-
+      <div>
+        <Header></Header>
+        <div className="wrapper">
+        </div>
+        <Footer></Footer>
       </div>
     );
   }
