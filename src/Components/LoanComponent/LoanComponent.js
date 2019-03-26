9cc6f2c9cff21225cbfb7dbeea6199f1d996ce45
hemant.gogoi <hemant.gogoi@credr.com>
2019-03-26 22:16:11 +0530

    fix() : bootstrap 4 fixes

--- src/Components/LoanComponent/LoanComponent.js
+++ src/Components/LoanComponent/LoanComponent.js
@@ -5,14 +5,34 @@ class LoanComponent extends Component {
     render() {
         return (
             <div className="row">
-                <div className="col-sm-12">
-                    <div className="card">
-                        <div className="card-body">
-                            <h1>Loan Calculator</h1>
-                            <div class="hrLine"></div>
+                <div className="card">
+                    <div className="card-body">
+                        <h1>Loan Calculator</h1>
+                        <div className="hrLine"></div>
+                        <div className="row content">
+                            <div className="col-sm-4">
+                                <label className="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
+                            </div>
+                            <div className="col-sm-8">
+                                <input type="email" className="form-control" id="inputEmail3" placeholder="Email">
+                                </input>
+                            </div>
+                        </div>
+                        <div className="row content">
+                            <div className="col-sm-4">
+                                Tenure
+                                </div>
+                            <div className="col-sm-4">
+                                Traanm
+                                </div>
+                        </div>
+                        <div className="row content">
+                        </div>
+                        <div className="row content">
                         </div>
                     </div>
                 </div>
+
             </div>
         )
     }
